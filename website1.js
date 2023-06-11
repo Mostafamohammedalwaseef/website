69f8c5ecb89ef0d740ef73c1f9adc8d5f7264340
Mostafamohammedalwaseef <129695042+Mostafamohammedalwaseef@users.noreply.github.com>
2023-06-12 00:14:00 +0300

    Add files via upload

---
+++ website1.js
@@ -0,0 +1,7 @@
+document.querySelector("#btnlinks").addEventListener("click",() => {
+ 
+ if (document.querySelector(".links").style.display=="block") {
+  document.querySelector(".links").style.display="none"
+ } 
+ else{document.querySelector(".links").style.display="block"}
+})
